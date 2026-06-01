476e3f41b53079099925bcf5bcf70c19c74a6157
Kamil Brylew <kamil.brylew@nuclear-lab.com>
2026-06-01 14:48:46 +0200

    added auto device discovery and widget population

--- gui/controller/main_controller.py
+++ gui/controller/main_controller.py
@@ -12,6 +12,10 @@ from PySide6.QtWidgets import (
     QStatusBar,
     QWidget,
 )
+from serial.tools import list_ports
+
+from nlab_modbus.core.enums import DeviceType
+from nlab_modbus.discovery.scan import scan_local_modbus_devices, scan_remote_boards, scan_remote_modbus_devices
 
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 MAIN_WINDOW_UI = PROJECT_ROOT / "view" / "main_window.ui"
@@ -23,11 +27,14 @@ class ModbusMainWindow(QMainWindow):
         super().__init__(parent)
 
         self.ui = self._load_ui(MAIN_WINDOW_UI)
+        self.available_devices = {
+            "local": {},
+            "remote": {},
+        }
 
         self._setup_window()
         self._setup_menu_bar()
         self._setup_status_bar()
-
         self._connect_signals()
 
     def _load_ui(self, ui_path: Path) -> Any:
@@ -57,13 +64,15 @@ class ModbusMainWindow(QMainWindow):
         """
 
         self.setWindowTitle("Modbus Monitor")
-        self.resize(1100, 750)
+        # self.resize(1100, 750)
         self.setCentralWidget(self.ui)
+        self.ui.devices_group.hide()
 
         if APP_ICON.exists():
             self.setWindowIcon(QIcon(str(APP_ICON)))
         else:
             raise FileNotFoundError(f"Application icon not found: {APP_ICON}")
+        self.scan_local_ports()
 
     def _connect_signals(self) -> None:
         """
@@ -71,8 +80,10 @@ class ModbusMainWindow(QMainWindow):
         """
         self.action_about.triggered.connect(self.on_about_clicked)
         self.action_licenses.triggered.connect(self.on_licenses_clicked)
-
-        pass
+        self.action_scan_local_ports.triggered.connect(self.scan_local_ports)
+        self.ui.port_select.currentIndexChanged.connect(self._update_comboboxes)
+        self.ui.host_select.currentIndexChanged.connect(self._update_comboboxes)
+        self.ui.remote_port_select.currentIndexChanged.connect(self._update_comboboxes)
 
     def on_connect_clicked(self) -> None:
         """
@@ -107,6 +118,9 @@ class ModbusMainWindow(QMainWindow):
         self.action_exit.setShortcut("Ctrl+Q")
         file_menu.addAction(self.action_exit)
 
+        self.action_scan_local_ports = QAction("Scan for local ports")
+        connection_menu.addAction(self.action_scan_local_ports)
+
         self.action_clear_plot = QAction("&Clear Plot", self)
         view_menu.addAction(self.action_clear_plot)
 
@@ -120,6 +134,55 @@ class ModbusMainWindow(QMainWindow):
         self.setStatusBar(status_bar)
 
         self.statusBar().showMessage("Ready")
+
+    def scan_local_ports(self) -> None:
+        """Scan for available local COM ports"""
+        local_devices = scan_local_modbus_devices()
+        self.ui.port_select.clear()
+
+        for item in local_devices:
+            port = item["port"]
+            dev_id = item["device_id"]
+            dev_name = item["type"].name
+            if port not in self.available_devices["local"]:
+                self.available_devices["local"][port] = []
+
+            self.available_devices["local"][port].append(f"{dev_id} {dev_name}")
+
+        local_ports = list(self.available_devices["local"].keys())
+        self.ui.port_select.clear()
+        self.ui.port_select.addItems(local_ports)
+
+        self.ui.local_select.addItems(self.available_devices["local"][port])
+        found_devices = {}
+        ips = scan_remote_boards()
+        for ip in ips:
+            found_devices[ip] = {}
+            for port in ["5001", "5002"]:
+                found_devices[ip][port] = []
+                remotes = scan_remote_modbus_devices(ip, int(port))
+                for item in remotes:
+                    dev_id = item["device_id"]
+                    dev_name = item["type"].name
+                    if remotes:
+                        found_devices[ip][port].append(f"{dev_id} {dev_name}")
+        self.ui.host_select.clear()
+        self.ui.host_select.addItems(ips)
+
+        self.available_devices["remote"] = found_devices
+        self._update_comboboxes()
+
+    def _update_comboboxes(self):
+        local_port = self.ui.port_select.currentText()
+        local_ids = self.available_devices["local"][local_port]
+        self.ui.local_select.clear()
+        self.ui.local_select.addItems(local_ids)
+
+        remote_host = self.ui.host_select.currentText()
+        remote_port = self.ui.remote_port_select.currentText()
+        remote_ids = self.available_devices["remote"][remote_host][remote_port]
+        self.ui.remote_select.clear()
+        self.ui.remote_select.addItems(remote_ids)
 
     def on_about_clicked(self) -> None:
         QMessageBox.about(
